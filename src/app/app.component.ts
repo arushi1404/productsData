69853845d2f76c2cf6b246de710056d725867a5e
arushi1404 <47868889+arushi1404@users.noreply.github.com>
2022-07-23 17:36:59 +0530

    add products

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angularTest';
+export class AppComponent implements OnInit {
+ 
+  productsJson: any;
+  productsArray: any = [];
+
+  constructor(private http: HttpClient) { }
+  
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
+
+  getAllProducts() {
+    this.http.get('https://s3.amazonaws.com/open-to-cors/assignment.json').subscribe(res => {
+      let restemp = res;
+
+      console.log(restemp);
+      this.productsJson = restemp;
+
+      for (let key in this.productsJson.products) {
+
+        this.productsArray.push(this.productsJson.products[key]);
+
+      }
+
+      this.productsArray.sort((a: any, b: any) => {
+        return b.popularity - a.popularity;
+      });
+
+    })
+  }
 }
